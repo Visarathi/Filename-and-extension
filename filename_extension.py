7efb8efe7663e239f64afe21178b70e40187ce8f
Visarathi <70250592+Visarathi@users.noreply.github.com>
2020-11-17 14:15:51 +0530

    filename_extension.py
    Getting the file name from the user and printing its extension in the output.

---
+++ filename_extension.py
@@ -0,0 +1,10 @@
+#Getting the file name from the user and printing its extension:
+
+
+fN = input("enter the file name:")
+
+print(fN)
+
+ext = "py".split(".")[-1]
+
+print ("Extension of the file is : ",ext)
